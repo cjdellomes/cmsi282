a00e9cfa92a4ee22ff09527b81f76f459d055821
Chris Dellomes <cjdellomes@gmail.com>
2015-02-09 14:48:05 -0800

    Initial commit

---
+++ homework1/PriorityQueue.py
@@ -0,0 +1,49 @@
+class PriorityQueue:
+
+	def __init__(self):
+		self.heap = []
+
+	def add(self, x):
+		self.heap.append(x)
+		self.sift_up(len(self.heap) - 1)
+		return self
+
+	def peek(self):
+		return self.heap[0]
+
+	def remove(self):
+		if len(self.heap) == 0:
+			raise Exception()
+		elif len(self.heap) == 1:
+			self.heap = []
+		else:
+			self.heap[0] = self.heap.pop()
+			self.sift_down(0)
+
+	def sift_up(self, item):
+		parent = int((item - 1) / 2)
+		truth = self.heap[parent] > self.heap[item]
+		if parent >= 0 and truth:
+			temp = self.heap[parent]
+			self.heap[parent] = self.heap[item]
+			self.heap[item] = temp
+			self.sift_up(parent)
+
+	def sift_down(self, item):
+		child = (item * 2) + 1
+		if child >= len(self.heap):
+			return self
+		if child + 1 < len(self.heap) and self.heap[child] > self.heap[child+1]:
+			child += 1
+		if self.heap[item] > self.heap[child]:
+			temp = self.heap[child]
+			self.heap[child] = self.heap[item]
+			self.heap[item] = temp
+			self.sift_down(child)
+
+	def __len__(self):
+		return len(self.heap)
+
+	def __str__(self):
+		for i in range(0, len(self.heap)):
+			return str(self.heap)
